13270a7e52c3e5d0bfe24d3fd848815b2bfe14e9
sola-grj <1131631886@qq.com>
2024-07-20 16:42:39 +0800

    feat 商品分类页面骨架屏

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -5,6 +5,7 @@ import type { CategoryTopItem } from '@/types/category'
 import type { BannerItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { computed, ref } from 'vue'
+import PageSkeleton from './components/PageSkeleton.vue'
 
 // 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
@@ -20,11 +21,12 @@ const getCategoryTopData = async () => {
   const res = await getCategoryTopAPI()
   categoryList.value = res.result
 }
-
+// 页面是否加载完成 标识
+const isFinish = ref(false)
 // 页面加载
-onLoad(() => {
-  getBannerListData()
-  getCategoryTopData()
+onLoad(async () => {
+  await Promise.all([getBannerListData(), getCategoryTopData()])
+  isFinish.value = true
 })
 
 // 获取当前二级分类数据
@@ -34,7 +36,7 @@ const subCategoryList = computed(() => {
 </script>
 
 <template>
-  <view class="viewport">
+  <view class="viewport" v-if="isFinish">
     <!-- 搜索框 -->
     <view class="search">
       <view class="input">
@@ -85,6 +87,7 @@ const subCategoryList = computed(() => {
       </scroll-view>
     </view>
   </view>
+  <PageSkeleton v-else />
 </template>
 
 <style lang="scss">
